55d7ecb6753fad06acf7c151ddee4787133f3569
karanjot-gaidu <karanjot.gaidu@ontariotechu.net>
2024-11-03 05:41:55 -0500

    Added Grade Graph

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'grades_model.dart';
 import 'grade.dart';
 import 'grade_form.dart';
+import 'package:charts_flutter/flutter.dart' as charts;
 
 void main() {
   runApp(const MyApp());
@@ -44,6 +45,54 @@ class MyApp extends StatelessWidget {
       home: ListGrades(),
     );
   }
+}
+
+Widget _buildGradeDataTable(Map<String, int> gradeFrequencies) {
+  return DataTable(
+    columns: [
+      DataColumn(label: Text('Grade')),
+      DataColumn(label: Text('Frequency')),
+    ],
+    rows: gradeFrequencies.entries
+        .map(
+          (entry) => DataRow(
+        cells: [
+          DataCell(Text(entry.key)),
+          DataCell(Text(entry.value.toString())),
+        ],
+      ),
+    )
+        .toList(),
+  );
+}
+
+Widget _buildGradeBarChart(Map<String, int> gradeFrequencies) {
+  final data = gradeFrequencies.entries
+      .map((entry) => GradeFrequency(entry.key, entry.value))
+      .toList();
+
+  final series = [
+    charts.Series<GradeFrequency, String>(
+      id: 'Grades',
+      colorFn: (_, __) => charts.MaterialPalette.blue.shadeDefault,
+      domainFn: (GradeFrequency gradeFreq, _) => gradeFreq.grade,
+      measureFn: (GradeFrequency gradeFreq, _) => gradeFreq.frequency,
+      data: data,
+    )
+  ];
+
+  return charts.BarChart(
+    series,
+    animate: true,
+    vertical: true,
+  );
+}
+
+class GradeFrequency {
+  final String grade;
+  final int frequency;
+
+  GradeFrequency(this.grade, this.frequency);
 }
 
 class ListGrades extends StatefulWidget {
@@ -118,12 +167,52 @@ class _ListGradesState extends State<ListGrades> {
     _loadGrades();
   }
 
+  void _showGradeChart() {
+    final gradeFrequencies = _calculateGradeFrequencies();
+
+    showModalBottomSheet(
+      context: context,
+      builder: (context) {
+        return Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Text(
+                'Grade Frequencies',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 10),
+              _buildGradeDataTable(gradeFrequencies),
+              SizedBox(height: 20),
+              Expanded(child: _buildGradeBarChart(gradeFrequencies)),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  Map<String, int> _calculateGradeFrequencies() {
+    final frequencies = <String, int>{};
+    for (var grade in grades) {
+      frequencies[grade.grade] = (frequencies[grade.grade] ?? 0) + 1;
+    }
+    return Map.fromEntries(
+      frequencies.entries.toList()..sort((a, b) => a.key.compareTo(b.key)),
+    );
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('List of Grades'),
         actions: [
+          IconButton(
+            icon: Icon(Icons.bar_chart),
+            onPressed: _showGradeChart,
+          ),
           PopupMenuButton<String>(
             icon: Icon(Icons.sort),
             onSelected: _sortGrades,
